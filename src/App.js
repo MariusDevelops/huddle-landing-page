295b21f01727ebd1961714d86a72df97f567e1e9
MariusDevelops <brazasmarius@gmail.com>
2021-11-30 23:32:24 +0100

    Basic styling

--- src/App.js
+++ src/App.js
@@ -5,31 +5,38 @@ import image from "./images/illustration-mockups.svg";
 function App() {
   return (
     <main>
-      <section>
-        <Logo />
-        <Illustration />
-      </section>
-      <section>
-        <h1>
-          Build The Community <br /> Your Fans Will Love
-        </h1>
-        <p>
-          Huddle re-imagines the way we build communities. You have a voice, but
-          so does your audience. Create connections with your users as you
-          engage in genuine discussion.
-        </p>
-        <button>Register</button>
-        <i className="fab fa-facebook-f"></i>
-        <i className="fab fa-twitter"></i>
-        <i className="fab fa-instagram"></i>
-        {/* //h1 //p //button //icons */}
-      </section>
+      <div className="wrapper">
+        <section className="section1">
+          <div>
+            <Logo />
+          </div>
+          <div>
+            <Illustration />
+          </div>
+        </section>
+        <section className="section2">
+          <h1>
+            Build The Community <br /> Your Fans Will Love
+          </h1>
+          <p>
+            Huddle re-imagines the way we build communities. You have a voice,
+            but so does your audience. Create connections with your users as you
+            engage in genuine discussion.
+          </p>
+          <button>Register</button>
+          <div className="icons">
+            <i className="fab fa-facebook-f"></i>
+            <i className="fab fa-twitter"></i>
+            <i className="fab fa-instagram"></i>
+          </div>
+        </section>
+      </div>
       <footer>
         Challenge by{" "}
         <a href="https://www.frontendmentor.io?ref=challenge">
           Frontend Mentor
         </a>
-        . Coded by <a href="/">Your Name Here</a>.
+        . Coded by <a href="/">Marius</a>.
       </footer>
     </main>
   );
